7d9bd3ef4c2c6cce68ed8f687aef03dd89ed4ff4
wangzai1345 <2163017591@qq.com>
2025-05-31 21:37:39 +0800

    update css

--- craco.config.js
+++ craco.config.js
@@ -1,11 +1,16 @@
-const path = `require('path')`;
+/* eslint-disable @typescript-eslint/no-require-imports */
+const path = require('path');
+const CracoLessPlugin = require('craco-less');
 
 const resolve = dir => path.resolve(__dirname, dir);
 
 module.exports = {
+  plugins:[{plugin: CracoLessPlugin}],
   webpack: {
     alias: {
       '@': resolve('src'),
     },
   },
 };
+
+
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react';
+import React , { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import 'normalize.css';
+import '@/assets/css/index.less';
+import App from '@/App.tsx';
 
-import App from './App.tsx';
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
   </StrictMode>
 );
+
