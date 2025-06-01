eee9f9f2fc7b6659368952125b1d049d564f631c
wangzai1345 <2163017591@qq.com>
2025-06-01 09:39:24 +0800

    update router

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
   return (
     <>
        <div className='nav'>
-        <Link to="/discover">发现音乐</Link>
+        <Link to="/discover">发现</Link>
         <Link to="/download">下载</Link>
         <Link to="/focus">关注</Link>
         <Link to="/mine">我的</Link>
--- src/router/index.tsx
+++ src/router/index.tsx
@@ -10,21 +10,51 @@ import Discover from "@/pages/discover"; */
 
 const Download = lazy(() => import('@/pages/download'));
 const Focus = lazy(() => import('@/pages/focus'));
-const Mine = lazy(() => import('@/pages/mine'));
+const Mine = lazy(() => import('@/pages/discover/child-views/mine'));
 const Discover = lazy(() => import('@/pages/discover'));
-
+const Recommend = lazy(() => import('@/pages/discover/child-views/recommend'));
+const Album = lazy(() => import('@/pages/discover/child-views/album'));
+const Artist = lazy(() => import('@/pages/discover/child-views/artist'));
+const Djradio = lazy(() => import('@/pages/discover/child-views/djradio'));
+const Ranking = lazy(() => import('@/pages/discover/child-views/ranking'));
+const Songs = lazy(() => import('@/pages/discover/child-views/songs'));
 
 const routes: RouteObject[] = [
   {
     path: '/',
-    element: <Navigate to='/discover'/>
+    element: <Navigate to="discover"/>
   },
   {
     path:'/discover',
     element:<Discover />,
     children: [
       {
-        path: '',
+        path: '/discover',
+        element: <Navigate to='/discover/recommend' />
+      },
+      {
+        path: '/discover/recommend',
+        element: <Recommend />
+      },
+      {
+        path: '/discover/album',
+        element: <Album />
+      },
+      {
+        path: '/discover/artist',
+        element: <Artist />
+      },
+      {
+        path: '/discover/djradio',
+        element: <Djradio />
+      },
+      {
+        path: '/discover/ranking',
+        element: <Ranking />
+      },
+      {
+        path: '/discover/songs',
+        element: <Songs />
       }
     ]
   },
